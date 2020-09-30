e3d2098db99a3a578e197f5ad15ee33640646eed
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2020-09-30 18:52:49 +0200

    Added a method

---
+++ MyClass.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace OpenRepo
+{
+    public class MyClass
+    {
+        public async Task ExecuteNonQueryStoredProcedureAsync(DbContext context, string procedureName, int? commandTimeOut = null, params SqlParameter[] parameters)
+        {
+            DbConnection con = context.Database.GetDbConnection();
+
+            if (con.State == ConnectionState.Closed)
+            {
+                await con.OpenAsync();
+            }
+
+            using (SqlCommand command = (SqlCommand)con.CreateCommand())
+            {
+                var parametersForLog = parameters.Select(x => new { x.ParameterName, x.Value }).ToArray();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = procedureName;
+                command.Parameters.AddRange(parameters);
+                if (commandTimeOut.HasValue)
+                {
+                    command.CommandTimeout = commandTimeOut.Value;
+                }
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
